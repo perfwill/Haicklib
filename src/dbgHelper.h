bce0862450501f4b93ce052bc6d35cb813a30205
perfwill <perfwill@gmail.com>
2012-02-12 10:21:24 +0700

    add some more things, just skeletons

---
+++ src/dbgHelper.h
@@ -0,0 +1,7 @@
+#ifndef DBGHELPER_H
+#define DBGHELPER_H
+
+BEGIN_C_DECLS
+	void hckPrint(...);
+END_C_DECLS
+#endif
---
+++ src/haicklib.cpp
@@ -0,0 +1,5 @@
+#ifndef HAICKLIB_H
+#define HAICKLIB_H
+
+#include "dbgHelper.h"
+#endif
